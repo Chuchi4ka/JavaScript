e90b3e1df09515ad674946d2a21313ddab44647e
Anna Palamarchuk <ann100@ukr.net>
2024-03-10 17:32:30 +0200

    added new Java_HW24.js file

---
+++ JS HW 24.js
@@ -0,0 +1,21 @@
+//ДЗ 24
+//Варіант 1 while
+
+  let num = 1;
+  let sum = 0;
+  while(num <= 100){
+    if(num % 3 !==0){
+    sum = sum + num;
+  } num++
+} console.log("Total = " + sum);
+
+//Варіант 2 for
+
+let num = 1 
+let sum = 0
+
+for(; num <=100; num++){
+  if (num % 3 !== 0) {
+    sum += num
+  }
+} console.log("Total = " + sum)
