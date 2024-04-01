9d87e61eb62a8f7c2f2492ba168dc46dbcf1493e
Anna Palamarchuk <ann100@ukr.net>
2024-04-01 15:29:28 +0300

    added hw 17

---
+++ JS HW17.js
@@ -0,0 +1,25 @@
+ДЗ 17. Тусувати масив
+/* Ми знаємо як відсортувати масив. А ось спробуйте його перемішати!
+
+Приклад масиву: arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+Напишіть функцію myBlend(arr), яка перемішуватиме переданий їй масив. 
+Зверніть увагу: не створювати новий масив, вже перемішаний, а саме перемішувати той, який їй передали. 
+ */
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function myBlend(arr) {
+  for (let index = 0; index < arr.length; index++) {
+    let min = arr[0];
+    let max = arr.length;
+    let rnd = Math.floor(min + Math.random() * (max - min));
+    let startBland = arr[index];
+    arr[index] = arr[rnd];
+    arr[rnd] = startBland;
+  }
+}
+console.log("Before bland :", arr);
+myBlend(arr);
+console.log("After bland:", arr);
