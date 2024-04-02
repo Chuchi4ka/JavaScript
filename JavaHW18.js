d49cbc1e343aeb0b2238f1ec9c0f1da33dc358b1
Anna Palamarchuk <ann100@ukr.net>
2024-04-02 17:25:34 +0300

    added JSHW17.js file

---
+++ JavaHW18.js
@@ -0,0 +1,64 @@
+const company = {
+  name: 'Велика Компанія',
+  type: 'Головна компанія',
+  platform: 'Платформа для продажу квитків',
+  sellsSolution: 'Рішення для продажу квитків',
+  clients: [
+    {
+      name: 'Клієнт 1',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків',
+      partners: [
+        {
+          name: 'Клієнт 1.1',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+        },
+        {
+          name: 'Клієнт 1.2',
+          type: 'subSubCompany',
+          uses: 'Рішення для продажу квитків',
+          sells: 'Рішення для продажу квитків',
+          partners: [
+            {
+              name: 'Клієнт 1.2.3',
+              type: 'subSubCompany',
+              uses: 'Рішення для продажу квитків',
+              sells: 'Рішення для продажу квитків',
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Клієнт 2',
+      type: 'subCompany',
+      uses: 'ПО для продажу квитків',
+      sells: 'Рішення для продажу квитків'
+    }
+  ]
+};
+
+function findValueByKey(company, companyName) {
+  if (company.name === companyName) {
+  return company;
+  }
+  
+  for (const findClient of company.clients) {
+  if (findClient.name === companyName) {
+  return findClient;
+  }
+  if (findClient.partners) {
+  for (const partner of findClient.partners) {
+  const result = findValueByKey(partner, companyName);
+  if (result) {
+  return result;
+  }
+  }
+  }
+  }
+  return null;
+  }
+  console.log(findValueByKey(company, 'Клієнт 1.2.3'))
