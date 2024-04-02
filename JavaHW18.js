885eb60036405525d67a3a1f7544b26c8821087b
Chuchi4ka <150375291+Chuchi4ka@users.noreply.github.com>
2024-04-02 17:44:35 +0300

    Update JavaHW18.js

--- JavaHW18.js
+++ JavaHW18.js
@@ -1,64 +1,73 @@
 const company = {
-  name: 'Велика Компанія',
-  type: 'Головна компанія',
-  platform: 'Платформа для продажу квитків',
-  sellsSolution: 'Рішення для продажу квитків',
-  clients: [
-    {
-      name: 'Клієнт 1',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків',
-      partners: [
-        {
-          name: 'Клієнт 1.1',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-        },
-        {
-          name: 'Клієнт 1.2',
-          type: 'subSubCompany',
-          uses: 'Рішення для продажу квитків',
-          sells: 'Рішення для продажу квитків',
-          partners: [
-            {
-              name: 'Клієнт 1.2.3',
-              type: 'subSubCompany',
-              uses: 'Рішення для продажу квитків',
-              sells: 'Рішення для продажу квитків',
-            }
-          ]
-        }
-      ]
-    },
-    {
-      name: 'Клієнт 2',
-      type: 'subCompany',
-      uses: 'ПО для продажу квитків',
-      sells: 'Рішення для продажу квитків'
-    }
-  ]
-};
-
-function findValueByKey(company, companyName) {
-  if (company.name === companyName) {
-  return company;
-  }
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+      {
+        name: 'Клієнт 1',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків',
+        partners: [
+          {
+            name: 'Клієнт 1.1',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+          },
+          {
+            name: 'Клієнт 1.2',
+            type: 'subSubCompany',
+            uses: 'Рішення для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+              {
+                name: 'Клієнт 1.2.3',
+                type: 'subSubCompany',
+                uses: 'Рішення для продажу квитків',
+                sells: 'Рішення для продажу квитків',
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'Клієнт 2',
+        type: 'subCompany',
+        uses: 'ПО для продажу квитків',
+        sells: 'Рішення для продажу квитків'
+      }
+    ]
+  };
   
-  for (const findClient of company.clients) {
-  if (findClient.name === companyName) {
-  return findClient;
-  }
-  if (findClient.partners) {
-  for (const partner of findClient.partners) {
-  const result = findValueByKey(partner, companyName);
-  if (result) {
-  return result;
-  }
-  }
-  }
-  }
-  return null;
-  }
-  console.log(findValueByKey(company, 'Клієнт 1.2.3'))
+  function findValueByKey(companyName, company = {}) {
+    if (company.name === companyName) {
+    return company;
+    }
+    
+    for (const findClient of company.clients) {
+    if (findClient.name === companyName) {
+    return findClient;
+    }
+    if (findClient.partners) {
+    for (const partner of findClient.partners) {
+    // const result = findValueByKey(partner, companyName);
+    const result = findValueByKey(companyName, { clients: findClient.partners });
+    if (result) {
+    return result;
+    }
+    }
+    }
+    }
+    return null;
+    }
+    
+    console.log(findValueByKey('Клієнт 3', company));
+
+
+
+
+
+
+  
